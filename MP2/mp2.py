6dfabf2dd95711be9898526a52b3c38c8a6dc77f
AerrowV <businessasimk@gmail.com>
2025-09-04 11:02:01 +0200

    Co-authored-by: Jonas-Kestenholz <Jonas-Kestenholz@users.noreply.github.com> Co-authored-by: Zana <ShadowB0X@users.noreply.github.com>

--- MP2/mp2.py
+++ MP2/mp2.py
@@ -17,9 +17,9 @@ def load_wine(path):
             df = df.iloc[1:].reset_index(drop=True)
         return df
 
-red_df = load_wine("C:/Users/Asim/Downloads/MP1/MP2/winequality-red.xlsx")
-white_df = load_wine("C:/Users/Asim/Downloads/MP1/MP2/winequality-white.xlsx")
-wineqt_df = load_wine("C:/Users/Asim/Downloads/MP1/MP2/WineQT.csv")
+red_df = load_wine("C:/Users/busin/Documents/bi_assignments/MP2/winequality-red.xlsx")
+white_df = load_wine("C:/Users/busin/Documents/bi_assignments/MP2/winequality-white.xlsx")
+wineqt_df = load_wine("C:/Users/busin/Documents/bi_assignments/MP2/WineQT.csv")
 
 # ---Clean ---
 def clean(df):
@@ -36,7 +36,9 @@ def clean(df):
     df = df[[c for c in cols if c in df.columns]]
 
     for c in df.columns:
+        # Values that cant become numeric becomes 'NaN'
         df[c] = pd.to_numeric(df[c], errors="coerce")
+        # Here rows with 'NaN' gets deleted
     df = df.dropna()
 
     df = df.drop_duplicates().reset_index(drop=True)
@@ -74,9 +76,11 @@ plt.title("Distribution of Wine Quality Scores")
 plt.show()
 
 # --- Visual histogram for key features (positively skewed)
-all_wine[["alcohol", "residual_sugar", "volatile_acidity"]].hist(bins = 30, figsize = (10,5))
-plt.suptitle("Histograms of Key Features")
-plt.show
+cols = ["alcohol", "residual_sugar", "volatile_acidity"]
+for col in cols:
+    sb.histplot(all_wine[col], bins=30, kde=True)
+    plt.title(f"Histogram of {col.upper()}")
+    plt.show()
 
 # --- Correlation heatmap ---
 plt.figure(figsize=(12,8))
@@ -98,6 +102,18 @@ plt.show()
 
 # The dataset was already numeric and suitable for analysis. Apart from column name cleaning and removal of duplicates/IDs, no additional transformations were necessary.
 
+# Which type has higher average alcohol and residual sugar
+plt.figure(figsize=(8,6))
+sb.boxplot(data=all_wine, x="type", y="alcohol", palette="Set2")
+plt.title("Alcohol Levels by Wine Type")
+plt.show()
+
+plt.figure(figsize=(8,6))
+sb.boxplot(data=all_wine, x="type", y="residual_sugar", palette="Set2")
+plt.title("Residual Sugar by Wine Type")
+plt.show()
+
+
 # Discretize alcohol into bins
 all_wine["alcohol_bin"] = pd.cut(all_wine["alcohol"], bins=5,
                                  labels=["Very Low","Low","Medium","High","Very High"])
@@ -110,3 +126,17 @@ all_wine["quality_bin"] = pd.cut(all_wine["quality"],
 for col in ["alcohol","residual_sugar","volatile_acidity"]:
     stat, p = shapiro(all_wine[col])
     print(f"{col}: stat={stat:.3f}, p={p:.3f}")
+
+# Which other questions might be of interest for the wine consumers and which of wine distributers?
+
+# Consumers:
+# 1. If price data were available, is paying more strongly correlated with higher quality? (Price vs quality)
+# 2. Which chemical features influence taste the most? 
+# 3. Which wines tend to have lower sulphates / lower alcohol? Is there a “healthier” option within red vs white? (Health related)
+# 4. Which country does the wine stem from and does the origin of the wine have a correlation with the quality of the wine?
+
+# Wine distributers:
+# 1. What attributes drive higher-rated wines? Should they focus production on wines with higher alcohol and lower volatile acidity?
+# 2. Is there more variation in white wine quality vs red? (helps standardize production). 
+
+
